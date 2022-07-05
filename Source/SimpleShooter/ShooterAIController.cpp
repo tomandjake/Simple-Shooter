de65cb14c0d3f21a76e62510e5c628d8e54bd65b
tomandjake <1098222960@qq.com>
2022-07-05 19:29:53 +0800

    add Behavior Tree

--- Source/SimpleShooter/ShooterAIController.cpp
+++ Source/SimpleShooter/ShooterAIController.cpp
@@ -2,4 +2,30 @@
 
 
 #include "ShooterAIController.h"
+#include "Kismet/GameplayStatics.h"
+#include "BehaviorTree/BlackboardComponent.h"
 
+void AShooterAIController::BeginPlay()
+{
+    Super::BeginPlay();
+    APawn* PlayerPawn = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
+    // SetFocus(PlayerPawn);
+    if (AIBehavior != nullptr){
+        RunBehaviorTree(AIBehavior);
+        GetBlackboardComponent()->SetValueAsVector(TEXT("PlayerLocation"), PlayerPawn->GetActorLocation());
+    }    
+}
+
+void AShooterAIController::Tick(float DeltaTime)
+{
+    Super::Tick(DeltaTime);
+    // APawn* PlayerPawn = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
+    // if (LineOfSightTo(PlayerPawn)){
+    //     SetFocus(PlayerPawn);
+    //     MoveToActor(PlayerPawn,200);
+    // } else {
+    //     ClearFocus(EAIFocusPriority::Gameplay);
+    //     StopMovement();
+    // }
+
+}
--- Source/SimpleShooter/ShooterAIController.h
+++ Source/SimpleShooter/ShooterAIController.h
@@ -13,5 +13,11 @@ UCLASS()
 class SIMPLESHOOTER_API AShooterAIController : public AAIController
 {
 	GENERATED_BODY()
-	
+public:
+	virtual void Tick(float DeltaTime) override;
+protected:
+	virtual void BeginPlay() override;
+private:
+	UPROPERTY(EditAnywhere)
+	class UBehaviorTree* AIBehavior;
 };
